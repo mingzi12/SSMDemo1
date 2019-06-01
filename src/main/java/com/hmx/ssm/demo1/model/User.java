fe7c0ce14e4d0d20c37325865b207357c07e017e
mingzi12 <690632338@qq.com>
2019-06-01 11:16:32 +0800

    配置通用mapper时，数据库字段和实体类字段不一致时，遇到的问题，以及解决办法。

--- src/main/java/com/hmx/ssm/demo1/model/User.java
+++ src/main/java/com/hmx/ssm/demo1/model/User.java
@@ -5,7 +5,7 @@ import javax.persistence.Table;
 
 /**
  * @author xxxx
- * @description: User
+ * @description: 如果数据库表和实体类的名称一致（忽略大小写），则@Table可以不配置。
  * @date 2019/5/14 9:48
  */
 @Table(name = "user")
@@ -13,6 +13,10 @@ public class User {
 
     private int id;
 
+    /**
+     * 由于数据库中的字段是username,而实体类中的字段名称是userName，两者不对应，在通用mapper中拼接动态SQL是
+     *     userName会变成user_name,这样会导致SQL执行失败。解决办法是添加@Column注解，使两者对应起来
+     */
     @Column(name = "username")
     private String userName;
 
